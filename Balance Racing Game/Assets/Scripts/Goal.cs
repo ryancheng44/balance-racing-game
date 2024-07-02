e0707ca49d11a5c7cc7948267a159ca29c811de5
ryancheng44 <ryancheng44@gmail.com>
2024-07-02 11:02:28 -0400

    Added Balance Racing Game project

---
+++ Balance Racing Game/Assets/Scripts/Goal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collisionInfo)
+    {
+        if (collisionInfo.CompareTag("Player"))
+        {
+            Debug.Log("GAME WON! :D");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
